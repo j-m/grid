e6d58cb86a44c4c2a7acb8daa59fb718d3471864
Jonathan Marsh <hello@jonmarsh.dev>
2022-04-23 17:45:44 +0100

    feat(storage): use default value on fetch error
    Partial revert

--- javascript/settings/storage.mjs
+++ javascript/settings/storage.mjs
@@ -4,9 +4,13 @@ export function update(key, data) {
   localStorage.setItem(key, JSON.stringify(data))
 }
 
-export function retrieve(key) {
+export function retrieve(key, defaultValue) {
   try {
     const data = localStorage.getItem(key)
+    if (data === null) {
+      update(key, defaultValue)
+      return defaultValue
+    }
     return JSON.parse(data)
   } catch (e) {
     localStorageWorks = false
